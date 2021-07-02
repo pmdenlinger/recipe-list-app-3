bf9455d945e6e6c17fc1b34156a7fbaa201eb191
Paul Denlinger <pmdenlinger@icloud.com>
2021-07-01 18:40:36 -0700

    Initial Commit

---
+++ recipe-list-app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  recipe-list-app
+//
+//  Created by user on 7/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ recipe-list-app/recipe_list_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  recipe_list_appApp.swift
+//  recipe-list-app
+//
+//  Created by user on 7/1/21.
+//
+
+import SwiftUI
+
+@main
+struct recipe_list_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
